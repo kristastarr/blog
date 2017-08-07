69d4921c99a8d90b487e13e4d2dc4f2680bc07f4
kristastarr <kstarr0092@gmail.com>
2017-08-06 20:57:33 -0500

    add form to create new post, view to show post, and list of all posts on post index page

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,18 +1,32 @@
 class PostsController < ApplicationController
 
-#GET /posts
+
 	def index 
 		@posts = Post.all
 	end 	
 
-#POST /posts
-	def create
-
+	def show
+		@post = Post.find(params[:id])
 	end 	
 
-# Get /posts
-	def show
-		@posts = Post.find(params[:id])
+	def new
+		@post = Post.new 
 	end 
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+    		redirect_to @post
+  		else
+    		render 'new'
+  		end 
+	end 	
+
 		
 end 	
+
+private 
+	def post_params
+		params.require(:post).permit(:title, :text)
+	end 	
