c031819b5be4d151ecca24d2a3d10fee0755777c
kristastarr <kstarr0092@gmail.com>
2017-08-06 21:39:25 -0500

    add comments model, migration, and controller; add comment form to post show page

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807005618) do
+ActiveRecord::Schema.define(version: 20170807022656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 20170807005618) do
     t.datetime "updated_at"
   end
 
+  add_foreign_key "comments", "posts"
 end
